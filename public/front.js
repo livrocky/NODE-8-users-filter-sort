536444e7523df952eacfe48f4b6effdd5f3db472
rokas <rokasra@gmail.com>
2022-04-15 11:09:06 +0300

    sort btn working

--- public/front.js
+++ public/front.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 const BASE_URL = 'http://localhost:3002/api';
 const listEl = document.getElementById('list');
+const sortAgeBtn = document.getElementById('ageSort');
 
 function renderUsers(userArr) {
   const listString = userArr
@@ -16,11 +17,17 @@ function renderUsers(userArr) {
   listEl.innerHTML = listString;
 }
 
-async function getUsers() {
-  const resp = await fetch(`${BASE_URL}/users`);
+async function getUsers(urlEnd) {
+  const resp = await fetch(`${BASE_URL}/${urlEnd}`);
   const dataInJs = await resp.json();
   console.log('dataInJs===', dataInJs);
   renderUsers(dataInJs);
 }
 
-getUsers();
+getUsers('users');
+
+sortAgeBtn.addEventListener('click', () => {
+  // kai paspaudziam mygtuka pakeisti jame esanti zodeli ASC i DESC
+  getUsers('users/sort-age');
+  console.log('sorting');
+});
